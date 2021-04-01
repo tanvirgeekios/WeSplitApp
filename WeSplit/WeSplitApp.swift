9eccac6bc3c37e1b794b7fa859ce8ac52531905c
tanvirgeekios <tanvir.alam@iosventure.com>
2021-04-01 12:56:59 +0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by MD Tanvir Alam on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
